31ac1f692e80a1dd3e89f3c20ad0703015c552c7
WildBlizzard <ziyin064@gmail.com>
2021-01-25 11:12:04 +0800

    add new script

---
+++ rmRedSeal_zhi.py
@@ -0,0 +1,51 @@
+# --- 虽有多闻 若不修行 与不闻等 如人说食 终不能饱
+
+
+import cv2
+import sys
+import numpy as np
+from tools.completeOther import complete_it
+from tools.getFullPath import through_full_path
+from tools.originFileName import get_the_filename
+
+
+class RmRedSeal:
+
+
+    def __init__(self, image_data, save_to, index, thresh=160):
+        """
+        image_data: 完整输入路径
+        save_to: 输出路径
+        index: 输入路径长度
+        thresh: 脱粒值
+        """
+        self.image_data = image_data
+        self.save_to = save_to
+        self.index = index
+        self.thresh = thresh
+        origin_data = cv2.imread(self.image_data)
+        self.blue, self.green, self.red = cv2.split(origin_data)
+
+
+    def save_method(self, after_img):
+        """输出逻辑"""
+        out_where = complete_it(self.image_data, self.save_to, self.index)
+        file_name = get_the_filename(self.image_data, self.index, 'jpg')
+        cv2.imwrite(out_where[0] + file_name, after_img)
+
+
+    def main_process(self):
+        """主逻辑"""
+        if not self.image_data: return
+        # 转换图像
+        after = cv2.threshold(self.red, self.thresh, 255, cv2.THRESH_BINARY)[1]
+        return self.save_method(after)
+
+
+if __name__ == "__main__":
+    # 输入路径 输出路径 脱粒值
+    # 脱粒值应根据实际需求做出更改
+    input_way, save_way, thresh_way = sys.argv[1], sys.argv[2], sys.argv[3]
+    for img_data in (img for img in through_full_path(input_way)):
+        RmRedSeal(img_data, save_way, len(input_way), int(thresh_way)).main_process()
+    print('Process done')
